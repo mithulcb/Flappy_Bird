61d984cf1e012cc1271391a53f4a505581a83384
mithulcb <mithulcb@gmail.com>
2022-08-07 01:03:55 +0530

    Pushing incomplete

---
+++ Flappy Bird/Assets/Scripts/Player.cs
@@ -0,0 +1,131 @@
+﻿/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+
+
+    [SerializeField]
+    private float jumpForce = 11f;
+
+    private Rigidbody2D myBody;
+
+    private SpriteRenderer sr;
+
+    private Animator anim;
+
+    
+
+
+
+    private void Awake()
+    {
+
+        myBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        PlayerJump();
+       
+    }
+
+
+
+    void PlayerJump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            myBody.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+}*/
+
+
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    public Sprite[] sprites;
+    private int spriteIndex;
+
+    public float strength = 5f;
+    public float gravity = -9.81f;
+    public float tilt = 5f;
+
+    private Vector3 direction;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        InvokeRepeating(nameof(AnimateSprite), 1f, 1f);
+    }
+
+    private void OnEnable()
+    {
+        Vector3 position = transform.position;
+        position.y = 0f;
+        transform.position = position;
+        direction = Vector3.zero;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            direction = Vector3.up * strength;
+        }
+
+        // Apply gravity and update the position
+        direction.y += gravity * Time.deltaTime;
+        transform.position += direction * Time.deltaTime;
+
+       
+    }
+
+    private void AnimateSprite()
+    {
+        spriteIndex++;
+
+        if (spriteIndex >= sprites.Length)
+        {
+            spriteIndex = 0;
+        }
+
+        if (spriteIndex < sprites.Length && spriteIndex >= 0)
+        {
+            spriteRenderer.sprite = sprites[spriteIndex];
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Obstacle"))
+        {
+            FindObjectOfType<GameManager>().GameOver();
+        }
+        else if (other.gameObject.CompareTag("Score"))
+        {
+            FindObjectOfType<GameManager>().IncreaseScore();
+        }
+    }
+
+}
